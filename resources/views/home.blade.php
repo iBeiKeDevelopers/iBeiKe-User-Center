bc45af2f6f71487a6297695dd28f01842b2025db
ShuaiCheng Zhu <jason23347@gmail.com>
2020-08-23 16:48:21 +0800

    home: Modified style & Add password reset
    Signed-off-by: ShuaiCheng Zhu <jason23347@gmail.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,23 +9,23 @@
     <div class="row justify-content-center">
         <div class="col-12 col-lg-10 justify-content-center d-flex">
             <div id="user-card" class="col-12 m-auto">
-                <div class="col-sm-6 offset-sm-3 col-12 px-0">
+                <div class="col-lg-6 offset-lg-3 col-12 px-0">
                     <img src="/assets/top.png">
                 </div>
-                <div class="col-3 d-sm-block d-none px-0 float-left">
+                <div class="col-2 offset-1 d-lg-block d-none px-0 float-left">
                     <a href="http://city.ibeike.com" target="_blank">
                         <img alt="city.ibeike.com" src="/assets/left.png" />
                     </a>
                 </div>
-                <div id="user-card-body" class="col-sm-6 col-12 px-0 float-left">
+                <div id="user-card-body" class="col-lg-6 col-12 px-0 float-left">
                     {{-- logout --}}
                     <div class="col-8 offset-2">
                         <div class="user-card-logout float-right">
                             <a href="{{ route('logout') }}" onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
                                 <span>
-                                    <a-icon type="logout"></a-icon>
-                                    {{ __('Logout') }}
+                                    <a-icon type="logout" style="vertical-align: 0;"></a-icon>
+                                    <span style="vertical-align: text-top;">{{ __('Logout') }}</span>
                                 </span>
                             </a>
 
@@ -39,28 +39,55 @@
                         <img class="rounded-circle" src="{{ $user->avatarURL }}">
                     </div>
                     {{-- basic info --}}
-                    <div class="col-6 offset-3">
-                        <div class="user-card-header col-12">
+                    <div class="col-8 mx-auto">
+                        <div class="user-card-header col-8 offset-1">
                             <h4>{{ __('Basic Information') }}</h4>
                         </div>
                         <table class="user-card-table table table-hover">
                             <tr>
-                                <td class="text-right">{{ __('E-Mail Address') }}</td>
+                                <td class="text-center">
+                                    Mail
+                                    <br />
+                                    <a-icon type="mail" />
+                                </td>
                                 <td>{{ $user->email }}</td>
                             </tr>
                             <tr>
-                                <td class="text-right">{{ __('City username') }}</td>
+                                <td class="text-center">
+                                    City
+                                    <br />
+                                    <a-icon type="user"></a-icon>
+                                </td>
+
                                 <td>{{ $user->username }}</td>
                             </tr>
                             <tr>
-                                <td class="text-right">{{ __('PT Username') }}</td>
+                                <td class="text-center">
+                                    PT
+                                    <br />
+                                    <a-icon type="user"></a-icon>
+                                </td>
                                 <td>{{ $user->pt_name }}</td>
                             </tr>
                         </table>
+
+                        {{-- Secure Infomation --}}
+                        <div class="user-card-header col-8 offset-1">
+                            <h4>{{ __('Secure Information') }}</h4>
+                        </div>
+
+                        <div class="col-8 offset-2 col-lg-10 offset-lg-3">
+                            <div class="col-12">
+                                <a href="{{ route('password.update') }}" class="button button-link">
+                                    {{ __('Reset Password') }}
+                                </a>
+                            </div>
+                        </div>
                     </div>
+
                     <div class="col-5 offset-3" style="padding-top: 50px;"></div>
                 </div>
-                <div class="col-3 d-sm-block d-none px-0 float-right">
+                <div class="col-2 d-lg-block d-none px-0 float-left">
                     <a href="http://pt.ibeike.com" target="_blank">
                         <img alt="pt.ibeike.com" src="/assets/right.png" />
                     </a>
